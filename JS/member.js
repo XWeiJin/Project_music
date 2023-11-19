f400360eea41e0699dcf049c43a71091301a5239
JIN <apple26830763@gmail.com>
2023-11-19 22:12:01 +0800

    add div

---
+++ JS/member.js
@@ -0,0 +1,31 @@
+var argee;
+var user1;
+var pass1;
+$(function(){
+    $("#not").bind("click",function(){
+      location.href="./index.html";
+    })
+    $("#check").change(function(){
+    //   console.log($(this).val());
+      if($(this).is(':checked')){
+        console.log("同意");
+        // $(this).next().text("同意");
+        argee=true;
+      }else{
+        console.log("不同意");
+        // $(this).next().text("不同意");
+        argee=false;
+      }
+    })
+    if(argee==true){
+        $("#OK").bind("clink",function(){
+            console.log($("#user").val());
+
+
+        $("#show").html($("#user"));
+        });
+    }else{
+        alert("請同意服務條款");
+    }
+    
+  });
