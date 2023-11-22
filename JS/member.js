8b0265da44035d2f72fe85ca4877e9141bd67506
Jin <Jin@gmail.com>
2023-11-22 15:09:44 +0800

    js not

--- JS/member.js
+++ JS/member.js
@@ -2,30 +2,27 @@
 // var user1;
 // var pass1;
 // $(function(){
-//     $("#not").bind("click",function(){
-//       location.href="./index.html";
-//     })
-//     $("#check").change(function(){
-//     //   console.log($(this).val());
-//       if($(this).is(':checked')){
-//         console.log("同意");
-//         // $(this).next().text("同意");
-//         argee=true;
-//       }else{
-//         console.log("不同意");
-//         // $(this).next().text("不同意");
-//         argee=false;
-//       }
-//     })
-//     if(argee==true){
-//         $("#OK").bind("clink",function(){
+//     // $("#not").bind("click",function(){
+//     //   location.href="./index.html";
+//     // })
+//     // $("#check").change(function(){
+//     // //   console.log($(this).val());
+//     //   if($(this).is(':checked')){
+//     //     console.log("同意");
+//     //     // $(this).next().text("同意");
+//     //     argee=true;
+//     //   }else{
+//     //     console.log("不同意");
+//     //     // $(this).next().text("不同意");
+//     //     argee=false;
+//     //   }
+//     // })
+    
+//         $("#OK12").bind("clink",function(){
 //             console.log($("#user").val());
 
 
 //         $("#show").html($("#user"));
 //         });
-//     }else{
-//         alert("請同意服務條款");
-//     }
     
 //   });
