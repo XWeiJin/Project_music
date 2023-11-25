0cb7aa52fdec3cad0177ff862fed502e1ca8c1b6
JIN <apple26830763@gmail.com>
2023-11-25 21:46:33 +0800

    add member

--- JS/member.js
+++ JS/member.js
@@ -1,29 +1,39 @@
-// var argee;
-// var user1;
-// var pass1;
-// $(function(){
-// //     // $("#not").bind("click",function(){
-// //     //   location.href="./index.html";
-// //     // })
-// //     // $("#check").change(function(){
-// //     // //   console.log($(this).val());
-// //     //   if($(this).is(':checked')){
-// //     //     console.log("同意");
-// //     //     // $(this).next().text("同意");
-// //     //     argee=true;
-// //     //   }else{
-// //     //     console.log("不同意");
-// //     //     // $(this).next().text("不同意");
-// //     //     argee=false;
-// //     //   }
-// //     // })
-    
-//         $("#OK_bt").bind("clink",function(){
-//             // console.log("12");
-//             console.log($("#user").val());
-//             console.log($("#pass").val());
+var argee;
 
-//         $("#show").html($("#user"));
-//         });
+$(function(){
+    $("#bt_not").bind("click",function(){
+        console.log("not");
+        location.href="./member.html";
+    });
+
+    $("#checkbox").change(function(){
+        console.log($(this).val());
+        if($(this).is(':checked')){
+            console.log("同意");
+            argee = true;
+        }else{
+            console.log("不同意");
+            argee = false;
+        }
+    })
+
     
-//   });
+    $("#bt_OK").bind("click",function(){
+        if(argee==true){
+            $("#show").html("登入成功");
+            // if($("#user")=="text1234"||$("#pass")=="4321text"){
+            //     console.log("成功");
+            //     console.log($("#user").val());
+            //     console.log($("#pass").val());
+            // }else{
+            //     alert("帳號密碼錯誤");
+            // }
+        
+        }else{
+            alert("請同意服務條款");
+        }
+    });
+    
+    
+
+});
