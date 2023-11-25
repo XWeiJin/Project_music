365b8dd25cc53c1726cf9a2483bc837aa31fd875
JIN <apple26830763@gmail.com>
2023-11-26 00:13:26 +0800

    add mem+html

--- JS/member.js
+++ JS/member.js
@@ -20,14 +20,7 @@ $(function(){
     
     $("#bt_OK").bind("click",function(){
         if(argee==true){
-            $("#show").html("登入成功");
-            // if($("#user")=="text1234"||$("#pass")=="4321text"){
-            //     console.log("成功");
-            //     console.log($("#user").val());
-            //     console.log($("#pass").val());
-            // }else{
-            //     alert("帳號密碼錯誤");
-            // }
+            $("#show").html('登入成功');
         
         }else{
             alert("請同意服務條款");
